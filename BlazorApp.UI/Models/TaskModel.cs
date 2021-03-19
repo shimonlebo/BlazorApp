6902461b9a979f3aabb5035c4c90a65e4bc08045
Simon Lebovits <shimonlebo@gmail.com>
2021-03-19 10:25:57 -0700

    added task list

---
+++ BlazorApp.UI/Models/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.UI.Models
+{
+    public class TaskModel
+    {
+        public string Task { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
