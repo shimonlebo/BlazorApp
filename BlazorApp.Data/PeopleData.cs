0eca03e6b75c9881711c1bef6885cc0ab8577c03
Simon Lebovits <shimonlebo@gmail.com>
2021-03-18 20:24:38 -0700

    data access

---
+++ BlazorApp.Data/PeopleData.cs
@@ -0,0 +1,26 @@
+﻿using BlazorApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class PeopleData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public PeopleData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<PersonModel>> GetPeople()
+        {
+            string sql = "select * from dbo.People";
+
+            return _db.LoadData<PersonModel, dynamic>(sql, new { });
+        }
+    }
+}
